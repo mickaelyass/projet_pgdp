bc4d0c04d79504a53a1ec709e27129c435b9bba8
bonheur <yassegoungbeseton@gmail.com>
2024-06-08 00:03:37 +0100

    envoie du project pgdp 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConnexionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
